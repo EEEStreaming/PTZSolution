c7bbe35af2548cde7804add3a82b3ebc019152b0
chifteur <nschmied@gmail.com>
2020-06-17 16:26:45 +0200

    try to bind camera input status

--- PTZPadController/ViewModel/CameraViewModel.cs
+++ PTZPadController/ViewModel/CameraViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Messaging;
 using PTZPadController.BusinessLayer;
+using PTZPadController.Common;
 using PTZPadController.DataAccessLayer;
 using System;
 using System.Dynamic;
+using System.Threading.Tasks;
 
 namespace PTZPadController.ViewModel
 {
@@ -14,6 +16,7 @@ namespace PTZPadController.ViewModel
         private int _InputIndex = 0;
         private bool _Connected = false;
         private ICameraParserModel _Camera;
+        private CameraStatusEnum _SourceStatus;
         #endregion
 
         #region Properties
@@ -50,7 +53,15 @@ namespace PTZPadController.ViewModel
             }
         }
 
-        internal CameraStatusEnum SourceStatus { get; set; }
+        public CameraStatusEnum SourceStatus {
+            get { return _SourceStatus; }
+            set
+            {
+                if (_SourceStatus == value) return;
+                _SourceStatus = value;
+                RaisePropertyChanged("SourceStatus");
+            }
+        }
 
         #endregion
 
@@ -60,8 +71,25 @@ namespace PTZPadController.ViewModel
             _Camera = cam;
             _Name = cam.CameraName;
             _InputIndex = index;
+            _SourceStatus = CameraStatusEnum.Off;
             Connected = _Camera.Connected;
             MessengerInstance.Register<NotificationMessage<ISocketParser>>(this, SocketNotification);
+            MessengerInstance.Register<NotificationMessage<CameraEventArgs>>(this, CameraNotification);
+
+            Task.Delay(500).ContinueWith((t) => 
+            SourceStatus = CameraStatusEnum.Program
+            );
+        }
+
+        private void CameraNotification(NotificationMessage<CameraEventArgs> obj)
+        {
+            if (obj.Notification == ConstMessages.CameraStatusChanged)
+            {
+                if (obj.Content.CameraName == _Name)
+                {
+                    SourceStatus = obj.Content.Status;
+                }
+            }
         }
 
         private void SocketNotification(NotificationMessage<ISocketParser> msg)
