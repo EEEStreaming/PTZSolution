5460f7f9158710092e3f5b7faefe5152eda28f3a
chifteur <nschmied@gmail.com>
2020-11-07 16:37:30 +0100

    Add button on PresetsToolBox to see the camera focus Mode

---
+++ PTZPadController/Converter/FocusModeToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using PTZPadController.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PTZPadController.Converter
+{
+    public class FocusModeToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            ECameraFocusMode paramFocus = (ECameraFocusMode)parameter;
+            ECameraFocusMode valueFocus = (ECameraFocusMode)value;
+
+            return valueFocus == paramFocus ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
